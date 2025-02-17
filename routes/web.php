e2d32f6040ab7a554b774a19e703a7c4470a5fb8
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-02-17 14:30:31 +0700

    Praktikum Router - Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World' ;
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return 'NIM : 2341720007 Nama : Rafa Fadil Aras';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke - '.$postId." Komentar ke - ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id) {
+    return 'Halaman Artikel dengan Id '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function() {
+    //
+})->name('profile');
