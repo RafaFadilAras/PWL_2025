911cf87059195f5c50ea8b8986d8f0454d2ff849
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-02-23 09:38:41 +0700

    Modifikasi (add PageController)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -60,3 +61,6 @@ Route::get('/user/profile', function() {
 
 Route::get('/hello', [WelcomeController::class,'hello']);
 
+Route::get('/', [PageController::class,'index']);
+Route::get('/about', [PageController::class,'about']);
+Route::get('/articles/{id}', [PageController::class,'articles']);
