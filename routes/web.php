9cb51fc2b77ef882761f5b23bc18d717f6f9acda
Rafa Fadil Aras <rafafadilaras@gmail.com>
2025-02-23 11:29:57 +0700

    View dari Controller

--- routes/web.php
+++ routes/web.php
@@ -97,3 +97,5 @@ Route::resource('photos', PhotoController::class)->except([
 Route::get('/greeting', function () {
     return view('blog.hello', ['name' => 'Rafa Fadil Aras']);
 });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
